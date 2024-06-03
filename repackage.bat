3434580b07db25ddc30c055b3f8a4f1537d2aa35
dk865 <dkolb2056@icloud.com>
2024-06-03 17:29:49 -0400

    Adding Stuff...

--- repackage.bat
+++ repackage.bat
@@ -1,3 +1,4 @@
 @echo off
 del Packages
-python dpkg-scanpackages.py -o Packages debs
+python dpkg-scanpackages.py -a iphoneos-arm -o Packages.iphoneos-arm debs
+python dpkg-scanpackages.py -a iphoneos-arm64 -o Packages.iphoneos-arm64 debs
