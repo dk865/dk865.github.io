65836cf6818678206158b556c4d9e3f0029f5d88
dk865 <dkolb2056@icloud.com>
2024-06-03 17:51:36 -0400

    Fix Everything for a Temporary Thing

--- repackage.bat
+++ repackage.bat
@@ -1,4 +1,3 @@
 @echo off
 del Packages
-python dpkg-scanpackages.py -a iphoneos-arm -o Packages.iphoneos-arm debs
-python dpkg-scanpackages.py -a iphoneos-arm64 -o Packages.iphoneos-arm64 debs
+python dpkg-scanpackages.py -o Packages debs
