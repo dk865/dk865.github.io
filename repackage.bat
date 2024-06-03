b569dee02b9ce2f6225a88891a4c35b2a1a08baa
dk865 <dkolb2056@icloud.com>
2024-06-03 19:52:01 -0400

    Fixed Hopefully Cydia Repo End

--- repackage.bat
+++ repackage.bat
@@ -1,3 +1,6 @@
 @echo off
 del Packages
-python dpkg-scanpackages.py -o Packages debs
+del Packages.iphoneos-arm
+del iphoneos-arm64 -o Packages.iphoneos-arm6
+python dpkg-scanpackages.py -a iphoneos-arm -o Packages.iphoneos-arm debs
+python dpkg-scanpackages.py -a iphoneos-arm64 -o Packages.iphoneos-arm64 debs
